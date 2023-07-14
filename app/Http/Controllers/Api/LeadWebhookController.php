ec1d71af555dde54324d7e6b9fbc19120e5bbd8d
kulwinder <skulwinder@n2rtechnologies.in>
2023-07-14 13:03:47 +0530

    sent whatsapp message

--- app/Http/Controllers/Api/LeadWebhookController.php
+++ app/Http/Controllers/Api/LeadWebhookController.php
@@ -9,6 +9,7 @@ use App\Models\Lead;
 use App\Models\Fblead;
 use App\Models\Client;
 use Illuminate\Support\Facades\Hash;
+use Helper;
 
 class LeadWebhookController extends Controller
 {
@@ -52,82 +53,10 @@ class LeadWebhookController extends Controller
         $lead->geo_address       = $request->has('address') ? $request->address : '';
         $lead->save();
 
-        // $ch = curl_init();
-
-        // $mobile_no = $lead->phone;
-
-        // $params = [
-        //     "messaging_product" => "whatsapp", 
-        //     "recipient_type" => "individual", 
-        //     "to" => (strlen($mobile_no) <=10) ? '91'.$mobile_no : $mobile_no,
-        //     "type" => "template", 
-        //     "template" => [
-        //         "name" => "booking_voucher", 
-        //         "language" => [
-        //             "code" => "en_US"
-        //         ], 
-        //         "components" => [
-        //             [
-        //                 "type" => "header", 
-        //                 "parameters" => [
-        //                     [
-        //                         "type" => "text", 
-        //                         "text" => @$lead->name 
-        //                     ] 
-        //                 ]
-        //             ],
-        //             [
-        //                 "type" => "body", 
-        //                 "parameters" => [                           
-        //                     [
-        //                         "type" => "text", 
-        //                         "text" => @$name ?? 'Abhishek'
-        //                     ],
-        //                     [
-        //                         "type" => "text", 
-        //                         "text" => @$mobile ?? "919971717045"
-        //                     ],
-        //                 ] 
-        //             ],
-        //             [
-        //                 "type" => "button", 
-        //                 "sub_type" => "url",
-        //                 "index" => 0,
-        //                 "parameters" => [
-        //                     [
-        //                         "type" => "text", 
-        //                         "text" => @$path 
-        //                     ] 
-        //                 ] 
-        //             ] 
-        //         ] 
-        //     ] 
-        // ]; 
-
-        // curl_setopt($ch, CURLOPT_URL, 'https://graph.facebook.com/v16.0/'.env('WHATSAPP_API_CODE').'/messages');
-        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        // curl_setopt($ch, CURLOPT_POST, 1);
-        // curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
-
-        // $headers = array();
-        // $headers[] = 'Authorization: Bearer '.env('WHATSAPP_API_SECRET');
-        // $headers[] = 'Content-Type: application/json';
-        // curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-        // $result = curl_exec($ch);
-        // if (curl_errno($ch)) {
-        //     echo 'Error:' . curl_error($ch);
-        // }
-        // $data = json_decode($result, 1);
-
-        // curl_close($ch);
-        // if ($data && isset($data['error']) && !empty( $data['error'])) {
-
-           
-        // }
+          $result = Helper::sendWhatsappMessage($lead->phone,'hello_world',array('name'=>$lead->firstname));
          }
         return response()->json([
-            'message'       => 'Lead created successfully',            
+            'message'       => $lead,            
         ], 200);
 
     }
