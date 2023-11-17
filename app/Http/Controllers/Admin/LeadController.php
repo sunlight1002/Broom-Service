52ed56645176d9d17dcc50fd1e6de65a091c254f
fateh <gfateh@n2rtechnologies.in>
2023-11-17 16:40:51 +0530

    fix lead admin

--- app/Http/Controllers/Admin/LeadController.php
+++ app/Http/Controllers/Admin/LeadController.php
@@ -229,10 +229,12 @@ class LeadController extends Controller
         if ($validator->fails()) {
             return response()->json(['errors' => $validator->messages()]);
         }
+        $nm = explode(' ', $request->name);
+
 
         $lead                = Client::find($id);
-        $lead->firstname     = $request->firstname;
-        $lead->lastname      = $request->lastname;
+        $lead->firstname     = $nm[0];
+        $lead->lastname     = (isset($nm[1])) ? $nm[1] : '';
         $lead->phone         = $request->phone;
         $lead->email         = $request->email;
         $lead->geo_address   = $request->address;
