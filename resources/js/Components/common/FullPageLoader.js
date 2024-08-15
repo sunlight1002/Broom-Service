a5457a1d83d22b4af4dca98fbe6f78147904ede3
Ankit Vachhani <ankitvachhani38@gmail.com>
2024-08-15 17:32:11 +0530

    add new loader

--- resources/js/Components/common/FullPageLoader.js
+++ resources/js/Components/common/FullPageLoader.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "../../Assets/css/FullPageLoader.css";
+import gif from "./3dot.gif"
 
 const FullPageLoader = ({ visible = false }) => {
     return (
         visible && (
+            // <div className="loader-container">
+            //     <div className="loader"></div>
+            // </div>
             <div className="loader-container">
-                <div className="loader"></div>
+                <div className="w-full">
+                    <img src={gif} />
+                </div>
             </div>
         )
     );
