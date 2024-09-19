1116f30724be15b70e57b285d77014fe43f1e457
Ankit Vachhani <ankitvachhani38@gmail.com>
2024-09-19 10:55:01 +0530

    add client_meta table and fix commands

---
+++ app/Enums/ClientMetaEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enums;
+
+final class ClientMetaEnum extends AbstractEnum
+{
+    public const NOTIFICATION_SENT_24_HOURS = 'notification_sent_24_hour';
+    public const NOTIFICATION_SENT_3_DAY = 'notification_sent_3_day';
+    public const NOTIFICATION_SENT_7_DAY = 'notification_sent_7_day';
+}
