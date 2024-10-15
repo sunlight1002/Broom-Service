bf6e4649d47f9ed708c6665c9fc30c2e0be09802
Ankit Vachhani <ankitvachhani38@gmail.com>
2024-10-15 15:12:19 +0530

    fix another issue

--- app/Listeners/NotifyForLeadStatusChanged.php
+++ app/Listeners/NotifyForLeadStatusChanged.php
@@ -31,22 +31,169 @@ class NotifyForLeadStatusChanged implements ShouldQueue
      */
     public function handle(ClientLeadStatusChanged $event)
     {
-        // Notification::create([
-        //     'user_id' => $event->client->id,
-        //     'user_type' => get_class($event->client),
-        //     'type' => NotificationTypeEnum::CLIENT_LEAD_STATUS_CHANGED,
-        //     'status' => 'changed',
-        //     'data' => [
-        //         'new_status' => $event->newStatus
-        //     ]
-        // ]);
+        Notification::create([
+            'user_id' => $event->client->id,
+            'user_type' => get_class($event->client),
+            'type' => NotificationTypeEnum::CLIENT_LEAD_STATUS_CHANGED,
+            'status' => 'changed',
+            'data' => [
+                'new_status' => $event->newStatus
+            ]
+        ]);
 
-        // event(new WhatsappNotificationEvent([
-        //     "type" => WhatsappMessageTemplateEnum::CLIENT_LEAD_STATUS_CHANGED,
-        //     "notificationData" => [
-        //         'client' => $event->client,
-        //         'new_status' => $event->newStatus
-        //     ]
-        // ]));
+        if($event->newStatus === 'freeze client'){
+            event(new WhatsappNotificationEvent([
+               "type" => WhatsappMessageTemplateEnum::CLIENT_IN_FREEZE_STATUS,
+               "notificationData" => [
+                   'client' => $event->client->toArray(),
+               ]
+           ]));
+       }
+
+        if ($event->newStatus === 'uninterested') {
+
+            event(new WhatsappNotificationEvent([
+                "type" => WhatsappMessageTemplateEnum::FOLLOW_UP_ON_OUR_CONVERSATION,
+                "notificationData" => [
+                    'client' => $event->client->toArray(),
+                ]
+            ]));
+
+            event(new WhatsappNotificationEvent([
+                "type" => WhatsappMessageTemplateEnum::UNINTERESTED,
+                "notificationData" => [
+                    'client' => $event->client->toArray(),
+                ]
+            ]));
+
+            SendUninterestedClientEmail::dispatch($client, $emailData);
+        }
+
+        if ($event->newStatus === 'unanswered') {
+
+            event(new WhatsappNotificationEvent([
+                "type" => WhatsappMessageTemplateEnum::UNANSWERED_LEAD,
+                "notificationData" => [
+                    'client' => $event->client->toArray(),
+                ]
+            ]));
+        }
+
+        if ($event->newStatus === 'irrelevant') {
+
+            event(new WhatsappNotificationEvent([
+                "type" => WhatsappMessageTemplateEnum::INQUIRY_RESPONSE,
+                "notificationData" => [
+                    'client' => $event->client->toArray(),
+                ]
+            ]));
+
+            event(new WhatsappNotificationEvent([
+                "type" => WhatsappMessageTemplateEnum::IRRELEVANT,
+                "notificationData" => [
+                    'client' => $event->client->toArray(),
+                ]
+                ]));
+        };
+
+        if ($event->newStatus === 'pending') {
+
+            event(new WhatsappNotificationEvent([
+                "type" => WhatsappMessageTemplateEnum::PENDING,
+                "notificationData" => [
+                    'client' => $event->client->toArray(),
+                ]
+            ]));
+        };
+
+        if ($event->newStatus === 'potential') {
+
+            event(new WhatsappNotificationEvent([
+                "type" => WhatsappMessageTemplateEnum::POTENTIAL,
+                "notificationData" => [
+                    'client' => $event->client->toArray(),
+                ]
+            ]));
+        };
+
+        if ($event->newStatus === 'potential client') {
+
+            event(new WhatsappNotificationEvent([
+                "type" => WhatsappMessageTemplateEnum::POTENTIAL_CLIENT,
+                "notificationData" => [
+                    'client' => $event->client->toArray(),
+                ]
+            ]));
+        };
+
+        if ($event->newStatus === 'pending client') {
+
+            event(new WhatsappNotificationEvent([
+                "type" => WhatsappMessageTemplateEnum::PENDING_CLIENT,
+                "notificationData" => [
+                    'client' => $event->client->toArray(),
+                ]
+            ]));
+        };
+
+        if ($event->newStatus === 'waiting') {
+
+            event(new WhatsappNotificationEvent([
+                "type" => WhatsappMessageTemplateEnum::WAITING,
+                "notificationData" => [
+                    'client' => $event->client->toArray(),
+                ]
+            ]));
+        };
+
+        if ($event->newStatus === 'active client') {
+
+            event(new WhatsappNotificationEvent([
+                "type" => WhatsappMessageTemplateEnum::ACTIVE_CLIENT,
+                "notificationData" => [
+                    'client' => $event->client->toArray(),
+                ]
+            ]));
+        };
+
+        if ($event->newStatus === 'unhappy') {
+
+            event(new WhatsappNotificationEvent([
+                "type" => WhatsappMessageTemplateEnum::UNHAPPY,
+                "notificationData" => [
+                    'client' => $event->client->toArray(),
+                ]
+            ]));
+        };
+
+        if ($event->newStatus === 'price issue') {
+
+            event(new WhatsappNotificationEvent([
+                "type" => WhatsappMessageTemplateEnum::PRICE_ISSUE,
+                "notificationData" => [
+                    'client' => $event->client->toArray(),
+                ]
+            ]));
+        };
+
+        if ($event->newStatus === 'moved') {
+
+            event(new WhatsappNotificationEvent([
+                "type" => WhatsappMessageTemplateEnum::MOVED,
+                "notificationData" => [
+                    'client' => $event->client->toArray(),
+                ]
+            ]));
+        };
+
+        if ($event->newStatus === 'one-time') {
+
+            event(new WhatsappNotificationEvent([
+                "type" => WhatsappMessageTemplateEnum::ONETIME,
+                "notificationData" => [
+                    'client' => $event->client->toArray(),
+                ]
+            ]));
+        };
     }
 }
