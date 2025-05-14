6e34509b8768723643ae60c26e80c4072633bfbb
Ankit Vachhani <ankitvachhani38@gmail.com>
2025-05-14 11:46:52 +0530

    change days in unaswered reminder to 1, 3, 4

--- app/Enums/ClientMetaEnum.php
+++ app/Enums/ClientMetaEnum.php
@@ -16,11 +16,13 @@ final class ClientMetaEnum extends AbstractEnum
     public const NOTIFICATION_SENT_CONTRACT24HOUR = 'notification_sent_contract24hour';
     public const NOTIFICATION_SENT_CONTRACT3DAY = 'notification_sent_contract3day';
     public const NOTIFICATION_SENT_CONTRACT7DAY = 'notification_sent_contract7day';
-    public const STOP_NOTIFICATION = 'stop_notification';
+    
+    public const NOTIFICATION_SENT_UNANSWERED_1DAY = 'notification_sent_unanswered_1day';
     public const NOTIFICATION_SENT_UNANSWERED_3DAYS = 'notification_sent_unanswered_3days';
-    public const NOTIFICATION_SENT_UNANSWERED_7DAYS = 'notification_sent_unanswered_7days';
-    public const NOTIFICATION_SENT_UNANSWERED_8DAYS = 'notification_sent_unanswered_8days';
+    public const NOTIFICATION_SENT_UNANSWERED_4DAYS = 'notification_sent_unanswered_4days';
+    
     public const NOTIFICATION_SENT_CONTRACT_NOTSIGNED_24HOURS = 'notification_sent_contract_notsigned_24hours';
     public const NOTIFICATION_SENT_CONTRACT_NOTSIGNED_3DAYS = 'notification_sent_contract_notsigned_3days';
     public const NOTIFICATION_SENT_CONTRACT_NOTSIGNED_7DAYS = 'notification_sent_contract_notsigned_7days';
+    public const STOP_NOTIFICATION = 'stop_notification';
 }
