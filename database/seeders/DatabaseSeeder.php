df16d1b56c8a47e228fe614cbf36880c51409175
ArT <a.h.hovakimyan@gmail.com>
2025-06-15 11:24:30 +0400

    Fix telescope , Db seed and gitignore

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(AdminSeed::class);
+        $this->call(AdminSeed::class);
+        $this->call(AdminSeed::class);
         $this->call(ServiceSeed::class);
         $this->call(ServiceScheduleSeed::class);
         $this->call(CountrySeeder::class);
