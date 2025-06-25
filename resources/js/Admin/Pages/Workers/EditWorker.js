9f4f3c86e991f4d116481fcd82965b0e85df8025
Ankit Vachhani <ankitvachhani38@gmail.com>
2025-06-25 12:14:22 +0530

    add waitng in edit worker

--- resources/js/Admin/Pages/Workers/EditWorker.js
+++ resources/js/Admin/Pages/Workers/EditWorker.js
@@ -1114,6 +1114,7 @@ export default function EditWorker() {
                                             setItemStatus(e.target.value)
                                         }
                                     >
+                                        <option value="2">{t("worker.settings.waiting")}</option>
                                         <option value="1">{t("worker.settings.Enable")}</option>
                                         <option value="0">{t("worker.settings.Disable")}</option>
                                     </select>
